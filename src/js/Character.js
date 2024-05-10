6368e87d4646d880c79634b78c80820c89a17972
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2024-05-11 01:13:02 +0300

    init commit

---
+++ src/js/Character.js
@@ -0,0 +1,27 @@
+export default class Character {
+  constructor(name, type) {
+    if (name.length < 2 || name.length > 10 || typeof name !== 'string') {
+      throw new Error(
+        'Имя должно являться строкой и содержать от 2х до 10 символов'
+      );
+    }
+
+    const types = [
+      'Bowerman',
+      'Swordsman',
+      'Magician',
+      'Daemon',
+      'Undead',
+      'Zombie',
+    ];
+
+    if (!types.includes(type) || typeof type !== 'string') {
+      throw new Error('Введите допустимый тип персонажа.');
+    }
+
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+  }
+}
---
+++ src/js/__tests__/Character_test.js
@@ -0,0 +1,35 @@
+import Character from '../Character';
+
+test('creating the character class', () => {
+  const result = new Character('char', 'Bowerman');
+  const expected = {
+    health: 100,
+    level: 1,
+    name: 'char',
+    type: 'Bowerman',
+  };
+  expect(result).toEqual(expected);
+});
+
+test('Creating a class with a name of 1 character', () => {
+  expect(() => {
+    const c = new Character('c', 'Bowerman')
+    return c;
+  }).toThrow(new Error('Имя должно являться строкой и содержать от 2х до 10 символов'));
+});
+
+test('Creating a class with a name over 10 characters', () => {
+  expect(() => {
+    const qwertyuiopa = new Character('qwertyuiopa', 'Bowerman')
+    return qwertyuiopa;
+  }).toThrow(new Error('Имя должно являться строкой и содержать от 2х до 10 символов'));
+});
+
+test('Creating a class with a name over 10 characters', () => {
+  expect(() => {
+    const character = new Character('character', 'Assasin')
+    return character;
+  }).toThrow(new Error('Введите допустимый тип персонажа.'));
+});
+
+
